2bd63df2e1da4c5a2b0b9ecdf7e87fe136647f1d
Sadjow <sadjow@gmail.com>
2011-05-23 21:02:27 -0300

    Adicionando arquivos iniciais do projeto.

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+/* 
+ * (MIT License)
+ * Author: Sadjow Medeiros Leão <sadjow@gmail.com>
+ * Created on Monday - 23/05/2011
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace ZouBaseXNA
+{
+    /// <summary>
+    /// Primeira classe do projeto.
+    /// </summary>
+    /// <author>Sadjow</author>
+    public class Class1
+    {
+
+    }
+}
